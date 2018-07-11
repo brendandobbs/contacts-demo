48ffad084f4714d57d14bd9d93836f84dbe0decf
Dobbs, Brendan <brendan.dobbs@sap.com>
2018-07-11 07:24:20 +0100

    Add endpoint for processing events

--- server.js
+++ server.js
@@ -12,6 +12,34 @@ app.use('/graphql', GraphHttp({
     graphiql: true
 }));
 
+app.post('/events', function(req, res) {
+    console.log('Event received');
+    const event = parseEvent(req, res);
+    }
+);
+
 app.listen(APP_PORT, () => {
     console.log(`Server started on port ${APP_PORT}`);
 });
+
+function parseEvent(req, res)
+{
+    let data = req.body;
+    if (req.body.length > 0) {
+        if (req.get('content-type') === 'application/json') {
+            data = JSON.parse(req.body.toString('utf-8'))
+        } else {
+            data = req.body.toString('utf-8')
+        }
+    }
+    console.log('Event data = ' + data);
+    const event = {
+            'event-type': req.get('event-type'),
+            'event-id': req.get('event-id'),
+            'event-time': req.get('event-time'),
+            'event-namespace': req.get('event-namespace'),
+            data,
+            'extensions': { request: req, response: res },
+    };
+    return event;
+}
